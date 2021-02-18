85a310cb2e4643200c822d1599e11e625de9cca1
prgrm274 <programmer270487@gmail.com>
2021-02-18 11:47:16 +0700

    form1

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -26,8 +26,7 @@ function App() {
             backgroundColor: "blue"
           }}
         >
-          Halo
-        <button onClick={() => alert("hello")}>alert hello</button>
+        {/* <button onClick={() => alert("hello")}>alert hello</button> */}
         </div>
 
         <div>
--- my-app/my-app/src/form1.js
+++ my-app/my-app/src/form1.js
@@ -1,16 +1,29 @@
 // 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import styleddd from 'styled-components';
+
+const FormStyle1 = styleddd.div
+`
+border: 2px solid white;
+
+`;
+
+const date = '2021-02-18';
+const dateFormat = 'YYYY-DD-MM';
+
 class MyForm extends React.Component {
+
     render() {
         return (
-            <form>
-                <h1>Hello</h1>
-                <p>Enter your name:</p>
-                <input
-                    type="text"
-                />
-            </form>
+                <FormStyle1>
+                    <form>
+                        <input type="number" pattern="[0-9]*" maxLength={4}
+                            inputProps={{ maxLength: 4 }}
+                        />
+                    </form>
+                </FormStyle1>
         );
     }
 }
