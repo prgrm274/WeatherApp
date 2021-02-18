f799f6eee73c2d8dfbc3fc298e355ce659d4d9a1
prgrm274 <programmer270487@gmail.com>
2021-02-18 11:31:48 +0700

    call view from another class

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+import MyForm from './form1';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React!!!
+        </a> */}
+
+        {/*  */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "blue"
+          }}
+        >
+          Halo
+        <button onClick={() => alert("hello")}>alert hello</button>
+        </div>
+
+        <div>
+          <form>
+            <label>
+              <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />_
+            <label>
+              <input type="text" name="name" />
+            </label>_
+            <label>
+              <input type="text" name="name" />
+            </label>
+          </form>
+        </div>
+
+        <MyForm></MyForm>
+        <div className="MyForm">
+        </div>
       </header>
+
     </div>
   );
 }
+
+
+
+
+// 
 
 export default App;
