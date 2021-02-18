c53957ae23e38a3b8be029420ded33179e1b5c8b
prgrm274 <programmer270487@gmail.com>
2021-02-18 11:55:36 +0700

    font

--- my-app/my-app/src/form1.js
+++ my-app/my-app/src/form1.js
@@ -6,8 +6,9 @@ import styleddd from 'styled-components';
 
 const FormStyle1 = styleddd.div
 `
-border: 2px solid white;
-
+border: 1px solid white;
+background-color: gray;
+padding: 10px 0px 10px 0px;
 `;
 
 const date = '2021-02-18';
@@ -17,12 +18,22 @@ class MyForm extends React.Component {
 
     render() {
         return (
-                <FormStyle1>
-                    <form>
-                        <input type="number" pattern="[0-9]*" maxLength={4}
-                            inputProps={{ maxLength: 4 }}
-                        />
-                    </form>
+            <FormStyle1>
+                <form>
+                    <input type="number" pattern="[0-9]*" maxLength={4}
+                        inputProps={{ maxLength: 4 }}
+                    />
+                </form>
+                <form>
+                    <input type="number" pattern="[0-9]*" maxLength={2}
+                        inputProps={{ maxLength: 4 }}
+                    />
+                </form>
+                <form>
+                    <input type="number" pattern="[0-9]*" maxLength={2}
+                        inputProps={{ maxLength: 4 }}
+                    />
+                </form>
                 </FormStyle1>
         );
     }
